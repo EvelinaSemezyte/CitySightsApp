6311abc34d40adde45df91e503e6545f87935a39
Evelina Semezyte <evelina.semezyte@gmail.com>
2022-03-08 17:37:50 +0200

    - Add content model class - Add location manager delegate, location manager and Yelp API methods

--- City Sights App/ViewModels/ContentModel.swift
+++ City Sights App/ViewModels/ContentModel.swift
@@ -6,3 +6,97 @@
 //
 
 import Foundation
+import CoreLocation
+
+class ContentModel: NSObject, CLLocationManagerDelegate, ObservableObject  {
+    
+    var locationManager = CLLocationManager()
+    
+    override init() {
+        
+        // Init method of NSObject
+        super.init()
+        
+        // Set content model as the delegate of the location manager
+        locationManager.delegate = self
+        
+        // Request permission from the user
+        locationManager.requestWhenInUseAuthorization()
+        
+        
+    }
+    
+    // MARK: - Location Manager Delegate Methods
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        
+        if locationManager.authorizationStatus == .authorizedAlways ||
+            locationManager.authorizationStatus == .authorizedWhenInUse {
+            
+            // We have permission
+            // Start geolocating the user, after we get permission
+            locationManager.startUpdatingLocation()
+        }
+        else if locationManager.authorizationStatus == .denied {
+            // We don't have permission
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        // Gives us the location of the user
+        let userLocation = locations.first
+        
+        if userLocation != nil {
+            
+            // We have a location
+            // Stop requesting the location after we got it once
+            locationManager.stopUpdatingLocation()
+            
+            // TODO: If we have the coordinates of the user, send into Yelp APIv
+            //            getBusinesses(category: "arts", location: userLocation!)
+            getBusinesses(category: "restaurants", location: userLocation!)
+        }
+    }
+    
+    // MARK: - Yelp API methods
+    func getBusinesses(category: String, location: CLLocation) {
+        
+        // Create URL
+        /*
+         let urlString = "https://api.yelp.com/v3/businesses/search?latitude=\(location.coordinate.latitude)&longitude=\(location.coordinate.longitude)&categories=\(category)&limit=6"
+         let url = URL(string: urlString)
+         */
+        var urlComponents = URLComponents(string: "https://api.yelp.com/v3/businesses/search")
+        urlComponents?.queryItems = [
+            URLQueryItem(name: "latitude", value: String(location.coordinate.latitude)),
+            URLQueryItem(name: "longitude", value: String(location.coordinate.longitude)),
+            URLQueryItem(name: "categories", value: "category"),
+            URLQueryItem(name: "limit", value: "6")
+            
+        ]
+        let url = urlComponents?.url
+        
+        if let url = url {
+            
+            // Create URL Request
+            var request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10.0)
+            request.httpMethod = "GET"
+            request.addValue("Bearer \(ApiKey)", forHTTPHeaderField: "Authorization")
+          
+            // Get URLSession
+            let session = URLSession.shared
+            
+            // Create Data Task
+            let dataTask = session.dataTask(with: request) { data, response, error in
+                
+                // Check that there isn't an error
+                if error == nil {
+                    print(response)
+                }
+            }
+            
+            // Start the Data Task
+            dataTask.resume()
+        }
+    }
+}
